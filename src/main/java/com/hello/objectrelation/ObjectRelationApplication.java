153d0cfdf0b4f3a7f56ee450663b607db533dd19
JxxHxxx <leesin5498@naver.com>
2022-12-28 13:24:13 +0900

    Initial commit

---
+++ src/main/java/com/hello/objectrelation/ObjectRelationApplication.java
@@ -0,0 +1,13 @@
+package com.hello.objectrelation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjectRelationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ObjectRelationApplication.class, args);
+    }
+
+}
